48ad4f1ae4e05c56755be9d6e4ed709a07909f9b
xXGreeXx <30373622+xXGreeXx@users.noreply.github.com>
2020-01-23 17:10:28 -0500

    Commit

--- TadpoleProject/Assets/Scripts/Grapher.cs
+++ TadpoleProject/Assets/Scripts/Grapher.cs
@@ -16,25 +16,31 @@ public class Grapher : MonoBehaviour
         int length = data.Length;
 
         float widthScaleFactor = (width * 2) / (float)length / 2;
-        float heightScaleFactor = (height * 2) / (float)max / 2;
+        float heightScaleFactor = (height / 2) / (float)(max / 2 == 0 ? 1 : max / 2);
 
         int idx = 0;
         foreach (int dataPoint in data)
         {
-            Vector2 currentPoint = new Vector2((widthScaleFactor * idx) - (width / 2F) + widthScaleFactor / 2F, 0 - (height / 2F));
-            //Vector2 nextPoint = (idx + 1 < data.Length ? new Vector2((widthScaleFactor * (idx + 1)) - (width / 2F), (heightScaleFactor * data[idx + 1]) - (height / 2F)) : currentPoint);
-            Vector2 nextPoint = new Vector2((widthScaleFactor * idx) - (width / 2F) + widthScaleFactor / 2F, Mathf.Max((heightScaleFactor * dataPoint) - (height / 2F), 15 - (height / 2F)));
+            Vector2 currentPoint = new Vector2((widthScaleFactor * idx) - (width / 2F) + widthScaleFactor, (heightScaleFactor * data[idx]) - (height / 2F));
+            Vector2 nextPoint = (idx + 1 < data.Length ? new Vector2((widthScaleFactor * (idx + 1)) - (width / 2F) + widthScaleFactor, (heightScaleFactor * data[idx + 1]) - (height / 2F)) : currentPoint);
 
-            GameObject dataPointObject = GameObject.Instantiate(prefab);
+            GameObject dataPointObject = new GameObject();
             dataPointObject.name = "Data: " + idx;
             dataPointObject.transform.SetParent(panel.transform);
-            dataPointObject.transform.localPosition = currentPoint;
-            dataPointObject.transform.localScale = new Vector3(1, 1, 1);
-            dataPointObject.GetComponent<RectTransform>().sizeDelta = new Vector2(Vector2.Distance(currentPoint, nextPoint), widthScaleFactor);
 
-            Vector3 dir = currentPoint - nextPoint;
+            LineRenderer renderer = dataPointObject.AddComponent<LineRenderer>();
+            renderer.startWidth = 0.5F;
+            renderer.endWidth = 0.5F;
+            renderer.startColor = Color.black;
+            renderer.endColor = Color.black;
 
-            dataPointObject.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 180);
+            Vector3 newPosA = panel.transform.TransformPoint(currentPoint);
+            newPosA.z -= 1;
+            Vector3 newPosB = panel.transform.TransformPoint(nextPoint);
+            newPosB.z -= 1;
+
+            renderer.SetPosition(0, newPosA);
+            renderer.SetPosition(1, newPosB);
 
             idx++;
         }
